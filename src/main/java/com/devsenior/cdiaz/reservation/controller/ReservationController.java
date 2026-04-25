64e3b500af86cff0bbf22b7fbd6c2d498c0811b3
Cesar Augusto Diaz Arriaga <cesar.cursos80@gmail.com>
2026-04-25 17:26:50 -0500

    Clase 2

--- src/main/java/com/devsenior/cdiaz/reservation/controller/ReservationController.java
+++ src/main/java/com/devsenior/cdiaz/reservation/controller/ReservationController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/reservas")
 @RequiredArgsConstructor
